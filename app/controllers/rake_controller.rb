207beb0c57580923a022f977408cf2263412f736
Jelani Woods <jelani@firstdraft.com>
2025-04-30 11:15:52 -0500

    skip controller filters

--- app/controllers/rake_controller.rb
+++ app/controllers/rake_controller.rb
@@ -1,6 +1,8 @@
 class RakeController < ApplicationController
   require "rake"
   Rails.application.load_tasks
+  skip_before_action :set_current_user
+  skip_around_action :set_time_zone
 
   def db_seed
     # Re-enable the task if it has already been executed
