89ead02b6a82ce13dc18c13b1d7433d6a78ffea5
Raghu Betina <raghu@firstdraft.com>
2025-05-03 11:42:46 -0500

    Somehow this change got left behind

--- db/migrate/20250503162521_add_chat_users_count_to_chats.rb
+++ db/migrate/20250503162521_add_chat_users_count_to_chats.rb
@@ -1,7 +1,7 @@
 class AddChatUsersCountToChats < ActiveRecord::Migration[8.0]
   def up
     add_column :chats, :chat_users_count, :integer, default: 0, null: false
-    
+
     # Update existing records
     Chat.find_each do |chat|
       Chat.reset_counters(chat.id, :chat_users)
